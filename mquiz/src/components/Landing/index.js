b29cb6c33cbf163ea20773615afd0a08d86d5c98
jynadal <nadaljeanyves@hotmail.fr>
2020-10-05 23:30:13 +0200

    Set the events

--- mquiz/src/components/Landing/index.js
+++ mquiz/src/components/Landing/index.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, {useRef, useState, useEffect } from 'react'
 
 const Landing = () => {
+
+    const [btn, setBtn] = useState(false);
+    console.log(btn);
+
+    const refWolverine = useRef(null);
+    console.log(refWolverine);
+
+    useEffect(() => {
+
+        refWolverine.current.classList.add("startingImg");
+
+        setTimeout(() => {
+            refWolverine.current.classList.remove("startingImg");
+            setBtn(true)
+        }, 1500);
+    }, [])
+
+    const setLeftImg = () => {
+        refWolverine.current.classList.add("leftImg");
+    }
+    const setRightImg = () => {
+        refWolverine.current.classList.add("rightImg");
+    }
+    const clearImg = () => {
+        if(refWolverine.current.classList.contains("leftImg")) {
+            refWolverine.current.classList.remove("leftImg");
+        } else if(refWolverine.current.classList.contains("rightImg")) {
+            refWolverine.current.classList.remove("rightImg");
+        }
+    }
+    
+
+    const displayBtn = btn && (
+        <>
+           <div onMouseOver={setLeftImg} onMouseOut={clearImg} className="leftBox">
+                <button className="btn-welcome">Inscription</button>
+            </div>
+            <div onMouseOver={setRightImg} onMouseOut={clearImg} className="rightBox">
+                <button className="btn-welcome">Connexion</button>
+            </div>
+        </>
+    )
+
+
     return (
-        <main className="welcomePage">
-            
+        <main ref={refWolverine} className="welcomePage">
+
+        {displayBtn }
+
         </main>
     )
 }
