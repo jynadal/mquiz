59b39e2f5b64e0fbc14aa0ab68c3403155028a65
jynadal <nadaljeanyves@hotmail.fr>
2020-10-05 21:58:38 +0200

    M quiz CSS and  beggin

---
+++ mquiz/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-container">
+                <p>Projet réalisé par JY</p>
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ mquiz/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="banner-container">
+                <h1><a href="/">M Quiz</a></h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
